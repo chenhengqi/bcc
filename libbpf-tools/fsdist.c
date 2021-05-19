459c40649735b45745a67ed5a5178e2485e7d8ce
Hengqi Chen <chenhengqi@outlook.com>
2021-05-19 23:01:38 +0800

    update
    Signed-off-by: Hengqi Chen <chenhengqi@outlook.com>

--- libbpf-tools/fsdist.c
+++ libbpf-tools/fsdist.c
@@ -9,16 +9,26 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
+
 #include "fsdist.h"
 #include "fsdist.skel.h"
 #include "trace_helpers.h"
 
 #define warn(...) fprintf(stderr, __VA_ARGS__)
 
+enum fs_kind {
+	BTRFS,
+	EXT4FS,
+	NFS,
+	XFS,
+};
+
 static volatile sig_atomic_t exiting;
 
+static enum fs_kind fs_kind = EXT4FS;
 static bool emit_timestamp = false;
 static bool timestamp_in_ms = false;
 static pid_t target_pid = 0;
@@ -30,22 +40,23 @@ const char *argp_program_version = "fsdist 0.1";
 const char *argp_program_bug_address =
 	"https://github.com/iovisor/bcc/tree/master/libbpf-tools";
 const char argp_program_doc[] =
-"Summarize nfs operation latency.\n"
+"Summarize btrfs/ext4fs/nfs/xfs operation latency.\n"
 "\n"
-"Usage: fsdist [-h] [-T] [-m] [-p PID] [interval] [count]\n"
+"Usage: fsdist [-h] [-x] [-T] [-m] [-p PID] [interval] [count]\n"
 "\n"
 "EXAMPLES:\n"
-"    fsdist            # show operation latency as a histogram\n"
-"    fsdist -p 1216    # trace PID 1216 only\n"
-"    fsdist 1 10       # print 1 second summaries, 10 times\n"
-"    fsdist -m 5       # 5s summaries, milliseconds\n";
+"    fsdist                     # show ext4fs operation latency as a histogram\n"
+"    fsdist -x nfs -p 1216      # trace nfs operation with PID 1216 only\n"
+"    fsdist -x xfs 1 10         # trace xfs operation, print 1 second summaries, 10 times\n"
+"    fsdist -x btrfs -m 5       # trace btrfs operation, 5s summaries, in milliseconds\n";
 
 static const struct argp_option opts[] = {
-	{"timestamp", 'T', NULL, 0, "Print timestamp"},
-	{"milliseconds", 'm', NULL, 0, "Millisecond histogram"},
-	{"pid", 'p', "PID", 0, "Process PID to trace"},
-	{"verbose", 'v', NULL, 0, "Verbose debug output"},
-	{NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help"},
+	{ "timestamp", 'T', NULL, 0, "Print timestamp" },
+	{ "milliseconds", 'm', NULL, 0, "Millisecond histogram" },
+	{ "pid", 'p', "PID", 0, "Process PID to trace" },
+	{ "fs", 'x', "Filesystem", 0, "Which filesystem to trace, [btrfs/ext4fs/nfs/xfs]" },
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
 	{},
 };
 
@@ -62,6 +73,20 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 		break;
 	case 'm':
 		timestamp_in_ms = true;
+		break;
+	case 'x':
+		if (!strcmp(arg, "btrfs")) {
+			fs_kind = BTRFS;
+		} else if (!strcmp(arg, "ext4fs")) {
+			fs_kind = EXT4FS;
+		} else if (!strcmp(arg, "nfs")) {
+			fs_kind = NFS;
+		} else if (!strcmp(arg, "xfs")) {
+			fs_kind = XFS;
+		} else {
+			warn("invalid filesystem\n");
+			argp_usage(state);
+		}
 		break;
 	case 'p':
 		errno = 0;
@@ -142,6 +167,115 @@ static int print_hists(struct fsdist_bpf__bss *bss)
 	return 0;
 }
 
+static void disable_autoload(struct fsdist_bpf *obj)
+{
+	int i;
+
+	for (i = 0; i < obj->skeleton->prog_cnt; i++) {
+		bpf_program__set_autoload(*obj->skeleton->progs[i].prog, false);
+	}
+}
+
+static void enable_btrfs_probes(struct fsdist_bpf *obj)
+{
+	if (fentry_exists("btrfs_file_read_iter", "btrfs")) {
+		bpf_program__set_autoload(obj->progs.btrfs_file_read_fentry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_read_fexit, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_write_fentry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_write_fexit, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_open_fentry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_open_fexit, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_sync_fentry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_sync_fexit, true);
+	} else {
+		bpf_program__set_autoload(obj->progs.btrfs_file_read_entry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_read_return, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_write_entry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_write_return, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_open_entry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_open_return, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_sync_entry, true);
+		bpf_program__set_autoload(obj->progs.btrfs_file_sync_return, true);
+	}
+}
+
+static void enable_ext4fs_probes(struct fsdist_bpf *obj)
+{
+	if (fentry_exists("ext4_file_read_iter", NULL)) {
+		bpf_program__set_autoload(obj->progs.ext4_file_read_fentry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_read_fexit, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_write_fentry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_write_fexit, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_open_fentry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_open_fexit, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_sync_fentry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_sync_fexit, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_getattr_fentry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_getattr_fexit, true);
+	} else {
+		bpf_program__set_autoload(obj->progs.ext4_file_read_entry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_read_return, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_write_entry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_write_return, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_open_entry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_open_return, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_sync_entry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_sync_return, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_getattr_entry, true);
+		bpf_program__set_autoload(obj->progs.ext4_file_getattr_return, true);
+	}
+}
+
+static void enable_nfs_probes(struct fsdist_bpf *obj)
+{
+	if (fentry_exists("nfs_file_read", "nfs")) {
+		bpf_program__set_autoload(obj->progs.nfs_file_read_fentry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_read_fexit, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_write_fentry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_write_fexit, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_open_fentry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_open_fexit, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_sync_fentry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_sync_fexit, true);
+		bpf_program__set_autoload(obj->progs.nfs_getattr_fentry, true);
+		bpf_program__set_autoload(obj->progs.nfs_getattr_fexit, true);
+	} else {
+		bpf_program__set_autoload(obj->progs.nfs_file_read_entry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_read_return, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_write_entry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_write_return, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_open_entry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_open_return, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_sync_entry, true);
+		bpf_program__set_autoload(obj->progs.nfs_file_sync_return, true);
+		bpf_program__set_autoload(obj->progs.nfs_getattr_entry, true);
+		bpf_program__set_autoload(obj->progs.nfs_getattr_return, true);
+	}
+}
+
+static void enable_xfs_probes(struct fsdist_bpf *obj)
+{
+	if (fentry_exists("xfs_file_read_iter", "xfs")) {
+		bpf_program__set_autoload(obj->progs.xfs_file_read_fentry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_read_fexit, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_write_fentry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_write_fexit, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_open_fentry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_open_fexit, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_sync_fentry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_sync_fexit, true);
+	} else {
+		bpf_program__set_autoload(obj->progs.xfs_file_read_entry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_read_return, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_write_entry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_write_return, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_open_entry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_open_return, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_sync_entry, true);
+		bpf_program__set_autoload(obj->progs.xfs_file_sync_return, true);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	static const struct argp argp = {
@@ -176,28 +310,20 @@ int main(int argc, char **argv)
 	skel->rodata->target_pid = target_pid;
 	skel->rodata->in_ms = timestamp_in_ms;
 
-	if (!fentry_exists("nfs_file_read", "nfs")) {
-		bpf_program__set_autoload(skel->progs.nfs_file_read_fentry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_read_fexit, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_write_fentry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_write_fexit, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_open_fentry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_open_fexit, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_sync_fentry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_sync_fexit, false);
-		bpf_program__set_autoload(skel->progs.nfs_getattr_fentry, false);
-		bpf_program__set_autoload(skel->progs.nfs_getattr_fexit, false);
-	} else {
-		bpf_program__set_autoload(skel->progs.nfs_file_read_entry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_read_return, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_write_entry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_write_return, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_open_entry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_open_return, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_sync_entry, false);
-		bpf_program__set_autoload(skel->progs.nfs_file_sync_return, false);
-		bpf_program__set_autoload(skel->progs.nfs_getattr_entry, false);
-		bpf_program__set_autoload(skel->progs.nfs_getattr_return, false);
+	disable_autoload(skel);
+	switch (fs_kind) {
+	case BTRFS:
+		enable_btrfs_probes(skel);
+		break;
+	case EXT4FS:
+		enable_ext4fs_probes(skel);
+		break;
+	case NFS:
+		enable_nfs_probes(skel);
+		break;
+	case XFS:
+		enable_xfs_probes(skel);
+		break;
 	}
 
 	err = fsdist_bpf__load(skel);
